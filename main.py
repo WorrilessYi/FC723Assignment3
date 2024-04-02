d76eefa6adc0cafc82c793dd0dfcd2b199f5161d
WorrilessYi <148446839+WorrilessYi@users.noreply.github.com>
2024-04-02 16:37:23 +0100

    fixed input validation and added error message

--- main.py
+++ main.py
@@ -11,17 +11,34 @@ def GCD(a, b):
         remainder = a % b
         # call the function again
         return GCD(b, remainder)
+
+def error_message():
+    print("Please enter a positive integer!!")
 def user_interface():
     print("Welcome to the relative prime number calculator!")
     while True:
-        num1 = input("Please enter the first number: ")
-        while not num1.isnumeric():
-            num1 = input("Please enter valid number: ")
-        num1 = int(num1)
-        num2 = input("Please enter the second number: ")
-        while not num2.isnumeric():
-            num2 = input("Please enter valid number: ")
-        num2 = int(num2)
+        while True:
+            num1 = input("num1: ")
+            try:
+                num1 = int(num1)
+            except ValueError:
+                error_message()
+            else:
+                if num1 >= 0:
+                    break
+                else:
+                    error_message()
+        while True:
+            num2 = input("num2: ")
+            try:
+                num2 = int(num2)
+            except ValueError:
+                error_message()
+            else:
+                if num2 >= 0:
+                    break
+                else:
+                    error_message()
         common_divisor = GCD(num1, num2)
         if common_divisor == 1:
             print(f"{num1} and {num2} are relative primes, the GCD is {common_divisor}")
@@ -29,7 +46,7 @@ def user_interface():
             print(f"{num1} and {num2} are relative primes")
         else:
             print(f"{num1} and {num2} are not relative primes")
-        ask = input("Do you want to calculate again?(no): ")
+        ask = input("\nDo you want to calculate again?(no): ")
         if ask.lower() == "no":
             break
     print("Thanks for using this calculator")
